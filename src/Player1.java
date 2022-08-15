73bf50c3ab07b32d68a5c618c3fb9d6b60e6a766
duc minh <ngominhduc100@gmail.com>
2022-08-15 21:40:35 +0700

    add feature lack bomb

--- src/Player1.java
+++ src/Player1.java
@@ -1,6 +1,7 @@
 import io.socket.emitter.Emitter;
 import jsclub.codefest.bot.constant.GameConfig;
 import jsclub.codefest.sdk.algorithm.AStarSearch;
+import jsclub.codefest.sdk.algorithm.BaseAlgorithm;
 import jsclub.codefest.sdk.model.Hero;
 import jsclub.codefest.sdk.socket.data.*;
 import jsclub.codefest.sdk.util.GameUtil;
@@ -13,7 +14,7 @@ public class Player1 {
 
     final static String SERVER_URL = "https://codefest.jsclub.me/";
     final static String PLAYER_ID = "player1-xxx";
-    final static String GAME_ID = "67615ab6-820e-4464-94bc-af63eec35a07";
+    final static String GAME_ID = "7d764cf0-1c1a-48cd-85ce-4ecf4ed5475e";
     final static Hero Player1 = new Hero(PLAYER_ID, GAME_ID);
 
     public static void main(String[] args) {
@@ -23,19 +24,19 @@ public class Player1 {
             map.updateMapInfo();
 
             List<Position> restrictPosition = getRestrictPosition(map);
-            String path = getPath(map, restrictPosition);
+            String path = AStarSearch.aStarSearch(map.mapMatrix,restrictPosition, map.getCurrentPosition(Player1),getTarget(map));
             Player1.move(path);
         };
-
         Player1.setOnTickTackListener(onTickTackListener);
         Player1.connectToServer(SERVER_URL);
     }
 
     public static List<Position> getRestrictPosition(MapInfo map) {
         List<Position> restrictPosition = new ArrayList<>();
-        //restrictPosition.addAll(map.balk);
+//        restrictPosition.addAll(map.balk);
         restrictPosition.addAll(map.walls);
         restrictPosition.addAll(map.teleportGate);
+        restrictPosition.addAll(getBomb(map));
         List<Position> virus = new ArrayList<>();
         for(Viruses vr:map.getVirus()) {
             virus.add(vr.position);
@@ -44,30 +45,50 @@ public class Player1 {
         return restrictPosition;
     }
 
-    public static String getPath(MapInfo map, List<Position> restrict) {
+    public static Position getTarget(MapInfo map) {
+        Position playerPosition = map.getPlayerByKey("player1-xxx").currentPosition;
+        Position target = playerPosition;
         if (map.spoils.size() != 0) {
-            Position target = map.spoils.get(0);
-            if (target != null) {
-                return AStarSearch.aStarSearch(map.mapMatrix, restrict, map.getCurrentPosition(Player1), target);
+            target = map.spoils.get(0);
+            for(int i = 1; i <  map.spoils.size(); i++)
+            {
+                if(BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) < 4 && BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) != 0) {
+                if(BaseAlgorithm.distanceBetweenTwoPoints(map.spoils.get(i), playerPosition) < BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) )
+                target = map.spoils.get(i);
+                    return target;
+                }
             }
         }
 
-        if (map.getHuman().size() != 0) {
-            Position target = null;
-            if (map.getPlayerByKey("player1-xxx").pill > 0) {
-                target = map.getHuman().get(0).position;
-            } else {
-                for (Human human : map.getHuman()) {
-                    if (!human.infected) {
-                        target = human.position;
-                        break;
-                    }
+        if (map.getPlayerByKey("player1-xxx").pill > 0) {
+            for(int i = 1; i <  map.getHuman().size(); i++)
+            {
+                if(BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) < 4 && BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) != 0) {
+                if(map.getHuman().get(i).infected && BaseAlgorithm.distanceBetweenTwoPoints(map.getHuman().get(i).position, playerPosition) < BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) )
+                    target = map.getHuman().get(i).position;
+                    return target;
                 }
             }
-            if (target != null) {
-                return AStarSearch.aStarSearch(map.mapMatrix, restrict, map.getCurrentPosition(Player1), target);
+        } else {
+            for(int i = 1; i <  map.getHuman().size(); i++)
+            {
+                if(BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) < 4 && BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) != 0)
+                if(!map.getHuman().get(i).infected && BaseAlgorithm.distanceBetweenTwoPoints(map.getHuman().get(i).position, playerPosition) < BaseAlgorithm.distanceBetweenTwoPoints(target, playerPosition) )
+                    target = map.getHuman().get(i).position;
+                    return target;
             }
         }
-            return "";
+        System.out.println(BaseAlgorithm.distanceBetweenTwoPoints(target,playerPosition));
+        return target;
+    }
+
+    public static List<Position> getBomb(MapInfo map) {
+        List<Position> bomb = new ArrayList<>();
+        for(Bomb item:map.bombs) {
+            for(int i = 0; i <= 4; i++) {
+                bomb.add(item.nextPosition(i,1));
+            }
+        }
+        return bomb;
     }
 }
